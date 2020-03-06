1273085ff40782f44344fa96d32d56c891f3622d
shubhanjayt <shubhanjay.tiwari_iot18@gla.ac.in>
2020-03-06 17:07:57 +0530

    Creating KeyPair generator object

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,13 @@
 package main;
 
+import java.security.KeyPairGenerator;
 import java.security.Signature;
 
 public class Main {
     public static class CipherDecrypt {
         public static void main(String args[]) throws Exception {
             Signature sign = Signature.getInstance("SHA256withRSA");
+            KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
 
 
         }
