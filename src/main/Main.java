ead7596dc01ff9974b7c4f3393adf231f0dd6432
shubhanjayt <shubhanjay.tiwari_iot18@gla.ac.in>
2020-03-06 17:05:20 +0530

    getInstance() mechanism to create Signature instance

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,12 @@
 package main;
 
+import java.security.Signature;
+
 public class Main {
     public static class CipherDecrypt {
         public static void main(String args[]) throws Exception {
+            Signature sign = Signature.getInstance("SHA256withRSA");
+
 
         }
     }
